dfde37b0164178fda48818c9a728a0bdd3d4c3ce
rheannemcintosh <rheanne.mcintosh@abdn.ac.uk>
2021-04-09 17:46:02 +0100

    Create a board for TicTacToe

---
+++ boards.py
@@ -0,0 +1,8 @@
+class Board:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.cells = []
+        for y in range(self.height):
+            for x in range(self.width):
+                self.cells.append((x, y))
