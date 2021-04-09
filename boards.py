45beab2c701ae69fad71b666c6af1295555d5397
rheannemcintosh <rheanne.mcintosh@abdn.ac.uk>
2021-04-09 17:56:31 +0100

    Define an iterable for the board

--- boards.py
+++ boards.py
@@ -7,6 +7,10 @@ class Board:
             for x in range(self.width):
                 self.cells.append((x, y))
 
+    def __iter__(self):
+        for cell in self.cells:
+            yield cell
+
 class TicTacToe(Board):
     def __init__(self, width=3, height=3):
         super().__init__(width, height)
